cbeaa1846e0a93e7fb74d2c31a679e84c5706a9d
Daniela Zapata <dzapatariesco@gmail.com>
2015-06-20 17:16:48 -0500

    Add default scope to order styles and ingredients by name

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,5 @@ class Ingredient < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 2, maximum: 25 }
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+  default_scope -> { order(name: :asc) }
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -2,4 +2,5 @@ class Style < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 2, maximum: 25 }
   has_many :recipe_styles 
   has_many :recipes, through: :recipe_styles
+  default_scope -> { order(name: :asc) }
 end
