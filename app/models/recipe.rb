2090680169e5ee6300f5fc2c19e539a0caad53db
Daniela Zapata <dzapatariesco@gmail.com>
2015-06-29 11:29:26 -0500

    Relax recipe description to allow 1000 characters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,7 +10,7 @@ class Recipe < ActiveRecord::Base
   validates :chef_id, presence: true
   validates :name, presence: true, length: { minimum: 5, maximum: 100 }
   validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
-  validates :description, presence: true, length: { minimum: 20, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 1000 }
   validate :picture_size
   default_scope -> { order(updated_at: :desc) }
 
